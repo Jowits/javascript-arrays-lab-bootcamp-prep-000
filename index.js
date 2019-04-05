92b2eb6e116309cd98c47e555c5c56820187ddd7
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-05 11:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function prependKitten(name) {
   return newKittens;
 }
 function removeLastKitten(){
-  var newKittens = kittens.slice(0, kittens.lenght -1);
+  var newKittens = kittens.slice(0, kittens.length -1);
   return newKittens;
 }
 function removeFirstKitten(){
