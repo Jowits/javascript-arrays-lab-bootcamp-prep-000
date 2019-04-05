15086645cf78215c2e92c4a49ed4d5740483e185
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-05 10:50:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyRemoveLastKitten(){
   kittens.pop();
   return kittens
 }
-function Arrays destructivelyRemoveFirstKitten(){
+function destructivelyRemoveFirstKitten(){
   kittens.shift();
   return kittens
 }
